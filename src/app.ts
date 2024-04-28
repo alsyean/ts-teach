61c30e5a1707fff8119db6b4ad2a6b341d166f12
alsyean <alsyean@gmail.com>
2024-04-28 16:10:28 +0900

    express middle ware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
-// @ts-ignore
-import * as express from 'express';
+import * as express from "express";
+import { Cat, CatType} from "./app.module";
 
-const app : express.Application = express()
-const port: number = 8000
+const app: express.Express = express();
+const port : number = 8000;
 
-app.get('/', (req : express.Request, res : express.Response) => {
-    res.send('Hello World!')
+app.use( (req, res, next ) => {
+    console.log(`middle ware`);
+    next();
+})
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send({ cats: Cat});
+})
+
+app.get('/cat/blue', (req: express.Request, res: express.Response) => {
+    res.send({ cats: Cat[0]});
+})
+
+app.use( (req, res, next ) => {
+    console.log(` this is error middle ware`);
+    res.send( {err: `404 not found error`})
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log('server start')
 })
